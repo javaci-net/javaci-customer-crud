7d9239e179f15f6998647aae720534c77680d09d
ozkansari <ozkansari@gmail.com>
2020-04-25 11:49:55 +0300

    Initial project from start.spring.io

---
+++ src/main/java/net/javaci/customerCrud/JavaciCustomerCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaci.customerCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaciCustomerCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaciCustomerCrudApplication.class, args);
+	}
+
+}
